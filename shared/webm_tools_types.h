c0139e6288498dd33b8e73ca3378bee8a8634292
Frank Galligan <fgalligan@google.com>
2012-01-20 16:19:59 -0500

    A command line tool to create WebM Dash manifest files.
    Squashed commit of the following:
    
    c0efd3d Fixed setting Period options.
    7f39e9a Style guide changes.
    57f03f9 Changed DashModel const pointer to const ref.
    52dd7ef Added const to some class member functions.
    1f74ba1 Removed some commented out code.
    631e48e Fix for PeakBandwidth bug.
    602c60e Fix bug with dash output.
    ea3ee1a Remove debug bandwidth output.
    a29a4f0 Added many features to webm_dash_manifest.
    e5f702e A command line tool to create WebM Dash manifest files.
    
    Change-Id: I343fd5fc4686e8a0784c0a1f74d98a99eed3328e

---
+++ shared/webm_tools_types.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2012 The WebM project authors. All Rights Reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree. An additional intellectual property rights grant can be found
+// in the file PATENTS.  All contributing project authors may
+// be found in the AUTHORS file in the root of the source tree.
+
+#ifndef SHARED_WEBM_TOOLS_TYPES_H_
+#define SHARED_WEBM_TOOLS_TYPES_H_
+
+// Copied from Chromium basictypes.h
+// A macro to disallow the copy constructor and operator= functions
+// This should be used in the private: declarations for a class
+#define WEBM_TOOLS_DISALLOW_COPY_AND_ASSIGN(TypeName) \
+  TypeName(const TypeName&);               \
+  void operator=(const TypeName&)
+
+namespace webm_tools {
+
+typedef unsigned char      uint8;
+typedef int                int32;
+typedef unsigned int       uint32;
+typedef long long          int64;
+typedef unsigned long long uint64;
+
+static const double kNanosecondsPerSecond = 1000000000.0;
+
+}  // namespace webm_tools
+
+#endif  // SHARED_WEBM_TOOLS_TYPES_H_
---
+++ webm_dash_manifest/period.h
@@ -0,0 +1,80 @@
+/*
+ *  Copyright (c) 2012 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef WEBM_DASH_MANIFEST_PERIOD_H_
+#define WEBM_DASH_MANIFEST_PERIOD_H_
+
+#include <string>
+#include <vector>
+
+#include "webm_tools_types.h"
+
+namespace indent_webm {
+class Indent;
+}  // namespace indent_webm
+
+namespace webm_dash {
+
+class AdaptationSet;
+
+class Period {
+ public:
+  explicit Period(const std::string& id);
+  ~Period();
+
+  // Calculates max duration.
+  bool Init();
+
+  // Add the AdaptationSet ids that will be contained within the Period.
+  void AddAdaptationSetID(const std::string& id);
+
+  // Returns the number of AdaptationSet ids.
+  int AdaptationSetIDSize() const;
+
+  // Returns AdaptationSet |id| referenced by |index|. Returns true if |id| is
+  // assigned.
+  bool AdaptationSetID(unsigned int index, std::string* id) const;
+
+  // Adds a AdaptationSet that is controlled by another object.
+  void AddAdaptationSet(const AdaptationSet* as);
+
+  // Outputs AdaptationSet in the prototype format.
+  void OutputDashManifest(FILE* o, indent_webm::Indent* indent) const;
+
+  double duration() const { return duration_; }
+  void set_duration(double duration) { duration_ = duration; }
+
+  std::string id() const { return id_; }
+  void set_id(const std::string& id) { id_ = id; }
+
+  double start() const { return start_; }
+  void set_start(double start) { start_ = start; }
+
+ private:
+  // Maximum duration of all |adaptation_sets_|.
+  double duration_;
+
+  // Used to differentiate between different Period within a MPD.
+  std::string id_;
+
+  // Start time in seconds for all of the AdaptationSets within this Period.
+  double start_;
+
+  // List of AdaptationSets for this Period.
+  std::vector<const AdaptationSet*> adaptation_sets_;
+
+  std::vector<std::string> adaptation_set_ids_;
+
+  WEBM_TOOLS_DISALLOW_COPY_AND_ASSIGN(Period);
+};
+
+}  // namespace webm_dash
+
+#endif  // WEBM_DASH_MANIFEST_PERIOD_H_
