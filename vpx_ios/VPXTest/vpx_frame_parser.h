d9c6f9ea4d6cbba3f7d43bce289c58e789d3aa5d
Tom Finegan <tomfinegan@google.com>
2014-07-16 15:51:29 -0700

    vpx_ios: Add VPXTest and testserver
    - VPXTest: A simple iOS app for testing libvpx performance.
    - testserver: A simple python server that provides file listings
                  and test files to VPXTest.
    
    Change-Id: Ifa8ae72fe6fe04b6a2df1512644a75ce3ab45573

---
+++ vpx_ios/VPXTest/vpx_frame_parser.h
@@ -0,0 +1,27 @@
+// Copyright (c) 2014 The WebM project authors. All Rights Reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree. An additional intellectual property rights grant can be found
+// in the file PATENTS.  All contributing project authors may
+// be found in the AUTHORS file in the root of the source tree.
+
+#ifndef VPXTEST_VPXTEST_VPX_FRAME_PARSER_H_
+#define VPXTEST_VPXTEST_VPX_FRAME_PARSER_H_
+
+#include <vector>
+
+#include "vpx_test_common.h"
+
+namespace VPXTest {
+
+class VpxFrameParser {
+ public:
+  virtual bool HasVpxFrames(const std::string &file_path,
+                            VpxFormat *vpx_format) = 0;
+  virtual bool ReadFrame(std::vector<uint8_t> *frame) = 0;
+};
+
+}  // namespace VPXTest
+
+#endif  // VPXTEST_VPXTEST_VPX_FRAME_PARSER_H_
