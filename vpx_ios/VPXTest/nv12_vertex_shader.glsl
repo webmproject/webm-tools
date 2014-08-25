043eb0e268936a26b0f2686bbce9421ad0e6eb81
Tom Finegan <tomfinegan@google.com>
2014-08-25 16:56:35 -0700

    vpx_ios/VPXTest: Wire up video playback.
    - Rendering is handled via a specialized GLKViewController.
    - OpenGLES 2.0 video rendering via two textures (and a shader to offload
      the YUV -> RGB heavy lifting onto the GPU).
    - No timing; plays at 60 frames per second (assuming the decoder keeps up).
    
    Change-Id: I0bf946dd91f781edcb9ff38bbb975c1db8676b26

---
+++ vpx_ios/VPXTest/nv12_vertex_shader.glsl
@@ -0,0 +1,16 @@
+// Copyright (c) 2014 The WebM project authors. All Rights Reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree. An additional intellectual property rights grant can be found
+// in the file PATENTS.  All contributing project authors may
+// be found in the AUTHORS file in the root of the source tree.
+attribute vec4 position;
+attribute vec2 texCoord;
+
+varying vec2 texCoordVarying;
+
+void main() {
+  gl_Position = position;
+  texCoordVarying = texCoord;
+}
