f434706cd71d0da68770564b60f935e9da73ae1e
Tom Finegan <tomfinegan@google.com>
2014-10-02 16:03:52 -0700

    vpx_ios/VPXTest: Add local playback mode, and make it the default.
    Change-Id: I7f3fbbe4ddb9f31b1561be80180b0062f10f1802

--- vpx_ios/VPXTest/vpx_test_common.h
+++ vpx_ios/VPXTest/vpx_test_common.h
@@ -9,6 +9,27 @@
 #ifndef VPXTEST_VPXTEST_VPX_TEST_COMMON_H_
 #define VPXTEST_VPXTEST_VPX_TEST_COMMON_H_
 
+// Comment the following line to enable usage of the basic webserver availabe in
+// vpx_ios/testserver/server.py via python, i.e.:
+// $ python path/to/webm-tools/vpx_ios/testserver/server.py
+//
+// Note: The above command must be run from a directory that contains IVF and/or
+// WebM files, and those files must contain VP8 or VP9 video streams.
+#define VPXTEST_LOCAL_PLAYBACK_ONLY
+
+#ifdef VPXTEST_LOCAL_PLAYBACK_ONLY
+#define kVp8File @"sintel_trailer_vp8.webm"
+#define kVp9File @"sintel_trailer_vp9.webm"
+#else
+// These values must be adjusted when running on an iOS device. localhost is not
+// the address you want on the device.
+#define TEST_SERVER @"http://localhost:8000"
+
+// Change allvpx to ivf to list only IVF files.
+// Change allvpx to webm to list only WebM files.
+#define TEST_FILE_LIST @"http://localhost:8000/allvpx"
+#endif
+
 namespace VpxTest {
 
 enum VpxCodec {
