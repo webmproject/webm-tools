a819a912318e55e839dc863d7ebf31069d24ff40
Tom Finegan <tomfinegan@google.com>
2015-07-29 15:54:55 -0700

    IxoDASHManifestParser: Add missing items to data model.
    Corrections:
    - Add Period to top level manifest class.
    - Add ID to Representation.
    - Make AdaptationSet a child of Period.
    - Add missing subsegmentStartsWithSAP to AdaptationSet.
    - Add missing startWithSAP to Representation.
    - Add audioChannelConfiguration to Representation.
    
    Refactor:
    - Don't bother with a wrapper type for representation storage; just
      use a mutable array in the period.
    
    Change-Id: I88a8dc77b5b71f755fb85ca4dd7036222ef69d93

--- vpx_ios/IxoPlayer/IxoDASHManifestParser.h
+++ vpx_ios/IxoPlayer/IxoDASHManifestParser.h
@@ -9,52 +9,50 @@
 
 #import "IxoDataSource.h"
 
-FOUNDATION_EXPORT NSString* const kMimeTypeWebmAudio;
-FOUNDATION_EXPORT NSString* const kMimeTypeWebmVideo;
-
-FOUNDATION_EXPORT NSString* const kCodecVP8;
-FOUNDATION_EXPORT NSString* const kCodecVP9;
-
-@interface IxoDASHRepresentationRecord : NSObject
-@property (nonatomic, strong) NSString* repID;
-@property (nonatomic) int bandwidth;
-@property (nonatomic) int width;
-@property (nonatomic) int height;
-@property (nonatomic, strong) NSString* baseURL;
-@property (nonatomic, strong) NSString* codecs;
-@property (nonatomic, strong) NSArray* segmentBase;
-@property (nonatomic, strong) NSArray* initialization;
-@end
-
-@interface IxoDASHRepresentationStore : NSObject
-@property (nonatomic, strong) NSMutableArray* representationRecords;
+@interface IxoDASHRepresentation : NSObject
+@property(nonatomic, strong) NSString* repID;
+@property(nonatomic) int bandwidth;
+@property(nonatomic) int width;
+@property(nonatomic) int height;
+@property(nonatomic) int audioSamplingRate;
+@property(nonatomic) int audioChannelConfig;
+@property(nonatomic) int startWithSAP;
+@property(nonatomic, strong) NSString* baseURL;
+@property(nonatomic, strong) NSString* codecs;
+@property(nonatomic, strong) NSArray* segmentBase;
+@property(nonatomic, strong) NSArray* initialization;
 @end
 
 @interface IxoDASHAdaptationSet : NSObject
-@property (nonatomic, strong) NSString* mimeType;
-@property (nonatomic, strong) NSString* codecs;
-@property (nonatomic) bool subsegmentAlignment;
-@property (nonatomic) bool bitstreamSwitching;
-@property (nonatomic) int width;
-@property (nonatomic) int height;
-@property (nonatomic, strong) IxoDASHRepresentationStore* representationStore;
+@property(nonatomic, strong) NSString* setID;
+@property(nonatomic, strong) NSString* mimeType;
+@property(nonatomic, strong) NSString* codecs;
+@property(nonatomic) bool subsegmentAlignment;
+@property(nonatomic) bool bitstreamSwitching;
+@property(nonatomic) int subsegmentStartsWithSAP;
+@property(nonatomic) int width;
+@property(nonatomic) int height;
+@property(nonatomic) int audioSamplingRate;
+@property(nonatomic, strong) NSMutableArray* representations;
 @end
 
 @interface IxoDASHPeriod : NSObject
-@property (nonatomic, strong) NSString* periodID;
-@property (nonatomic, strong) NSString* start;
-@property (nonatomic, strong) NSString* duration;
+@property(nonatomic, strong) NSString* periodID;
+@property(nonatomic, strong) NSString* start;
+@property(nonatomic, strong) NSString* duration;
+@property(nonatomic, strong) NSMutableArray* audioAdaptationSets;
+@property(nonatomic, strong) NSMutableArray* videoAdaptationSets;
 @end
 
 @interface IxoDASHManifest : NSObject
-@property (nonatomic, strong) NSString* mediaPresentationDuration;
-@property (nonatomic, strong) NSString* minBufferTime;
-@property (nonatomic) bool staticPresentation;
-@property (nonatomic, strong) NSMutableArray* audioAdaptationSets;
-@property (nonatomic, strong) NSMutableArray* videoAdaptationSets;
+@property(nonatomic, strong) NSString* mediaPresentationDuration;
+@property(nonatomic, strong) NSString* minBufferTime;
+@property(nonatomic) bool staticPresentation;
+@property(nonatomic, strong) IxoDASHPeriod* period;
 @end
 
 @interface IxoDASHManifestParser : NSObject<NSXMLParserDelegate>
 - (instancetype)initWithManifestURL:(NSURL*)manifestURL;
 - (bool)parse;
+
 @end
--- vpx_ios/IxoPlayer/IxoDASHManifestParser.m
+++ vpx_ios/IxoPlayer/IxoDASHManifestParser.m
@@ -13,34 +13,62 @@
 //
 // IxoDASHRepresentationRecord
 //
-@implementation IxoDASHRepresentationRecord
+@implementation IxoDASHRepresentation
 @synthesize repID = _repID;
 @synthesize bandwidth = _bandwidth;
 @synthesize width = _width;
 @synthesize height = _height;
+@synthesize audioSamplingRate = _audioSamplingRate;
+@synthesize audioChannelConfig = _audioChannelConfig;
+@synthesize startWithSAP = _startWithSAP;
 @synthesize codecs = _codecs;
 @synthesize baseURL = _baseURL;
 @synthesize segmentBase = _segmentBase;
 @synthesize initialization = _initialization;
-@end
 
-//
-// IxoDASHRepresentationStore
-//
-@implementation IxoDASHRepresentationStore
-@synthesize  representationRecords = _representationRecords;
+- (id)init {
+  self = [super init];
+  if (self) {
+    self.bandwidth = 0;
+    self.width = 0;
+    self.height = 0;
+    self.audioSamplingRate = 0;
+    self.segmentBase = [[NSArray alloc] init];
+    self.initialization = [[NSArray alloc] init];
+  }
+  return self;
+}
+
 @end
 
 //
 // IxoDASHAdaptationSet
 //
 @implementation IxoDASHAdaptationSet
+@synthesize setID = _setID;
 @synthesize mimeType = _mimeType;
 @synthesize codecs = _codecs;
 @synthesize subsegmentAlignment = _subsegmentAlignment;
 @synthesize bitstreamSwitching = _bitstreamSwitching;
+@synthesize subsegmentStartsWithSAP = _subsegmentStartsWithSAP;
 @synthesize width = _width;
 @synthesize height = _height;
+@synthesize audioSamplingRate = _audioSamplingRate;
+@synthesize representations = _representations;
+
+- (id)init {
+  self = [super init];
+  if (self) {
+    self.subsegmentAlignment = false;
+    self.bitstreamSwitching = false;
+    self.subsegmentStartsWithSAP = 0;
+    self.width = 0;
+    self.height = 0;
+    self.audioSamplingRate = 0;
+    self.representations = [[NSMutableArray alloc] init];
+  }
+  return self;
+}
 @end
 
 //
@@ -50,8 +78,19 @@
 @synthesize periodID = _periodID;
 @synthesize start = _start;
 @synthesize duration = _duration;
-@end
+@synthesize audioAdaptationSets = _audioAdaptationSets;
+@synthesize videoAdaptationSets = _videoAdaptationSets;
 
+- (id)init {
+  self = [super init];
+  if (self) {
+    self.audioAdaptationSets = [[NSMutableArray alloc] init];
+    self.videoAdaptationSets = [[NSMutableArray alloc] init];
+  }
+  return self;
+}
+
+@end
 
 //
 // IxoDASHManifest
@@ -60,8 +99,7 @@
 @synthesize mediaPresentationDuration = _mediaPresentationDuration;
 @synthesize minBufferTime = _minBufferTime;
 @synthesize staticPresentation = _staticPresentation;
-@synthesize audioAdaptationSets = _audioAdaptationSets;
-@synthesize videoAdaptationSets = _videoAdaptationSets;
+@synthesize period = _period;
 @end
 
 //
@@ -72,6 +110,12 @@
   NSURL* _manifestURL;
   NSLock* _lock;
   NSXMLParser* _parser;
+
+  IxoDASHManifest* _manifest;
+
+  // Parsing status.
+  bool _parseFailed;
+  NSMutableArray* _openElements;
 }
 
 - (id)init {
@@ -87,6 +131,9 @@
   if (self) {
     _lock = [[NSLock alloc] init];
     _manifestURL = manifestURL;
+    _openElements = [[NSMutableArray alloc] init];
+    _parseFailed = false;
+    _manifest = [[IxoDASHManifest alloc] init];
   }
   return self;
 }
@@ -119,6 +166,10 @@
   return [_parser parse] == YES;
 }
 
+- (bool) parsingCanContinue {
+  return false;
+}
+
 //
 // NSXMLParserDelegate
 //
@@ -131,19 +182,77 @@
        namespaceURI:(NSString*)namespaceURI
       qualifiedName:(NSString*)qName
          attributes:(NSDictionary*)attributeDict {
-  NSLog(@"didStartElement --> %@ \nwith attributeDict ---> \n%@", elementName,
+  NSLog(@"didStartElement (%@) \nwith attributeDict:\n%@", elementName,
         attributeDict);
+
+  if (_parseFailed) {
+    NSLog(@"didStartElement: Parsing stopped because _parseFailed.");
+    return;
+  }
+
+  if ([elementName isEqualToString:kElementMPD]) {
+    if (attributeDict == nil || [_openElements count] > 0) {
+      NSLog(@"didStartElement: _parseFailed (no MPD attribs/bad state).");
+      _parseFailed = true;
+      return;
+    }
+
+    [_openElements addObject:kElementMPD];
+
+    // Store MPD attributes.
+    _manifest.mediaPresentationDuration =
+        [attributeDict objectForKey:kAttributeMediaPresentationDuration];
+    _manifest.minBufferTime =
+        [attributeDict objectForKey:kAttributeMinBufferTime];
+    _manifest.staticPresentation =
+        [[attributeDict objectForKey:kAttributeType]
+            caseInsensitiveCompare:kPresentationTypeStatic] == NSOrderedSame;
+  } else if ([elementName isEqualToString:kElementPeriod]) {
+    if (attributeDict == nil || [_openElements count] < 1) {
+      NSLog(@"didStartElement: _parseFailed (no period attribs/bad state).");
+      _parseFailed = true;
+      return;
+    }
+  } else if ([elementName isEqualToString:kElementAdaptationSet]) {
+  } else if ([elementName isEqualToString:kElementRepresentation]) {
+  } else if ([elementName isEqualToString:kElementBaseURL]) {
+  } else if ([elementName isEqualToString:kElementSegmentBase]) {
+  } else if ([elementName isEqualToString:kElementInitialization]) {
+  } else {
+    // TODO(tomfinegan): Consider ignoring unknown elements instead of failing.
+    _parseFailed = true;
+    NSLog(@"didStartElement: _parseFailed (unknown element).");
+  }
 }
 
 - (void)parser:(NSXMLParser*)parser foundCharacters:(NSString*)string {
-  NSLog(@"foundCharacters --> %@", string);
+  NSLog(@"foundCharacters (%@)", string);
 }
 
 - (void)parser:(NSXMLParser*)parser
  didEndElement:(NSString*)elementName
   namespaceURI:(NSString*)namespaceURI
  qualifiedName:(NSString*)qName {
-  NSLog(@"didEndElement   --> %@", elementName);
+  NSLog(@"didEndElement (%@)", elementName);
+
+  if (_parseFailed) {
+    NSLog(@"Parsing stopped because _parseFailed in didEndElement.");
+    return;
+  }
+
+  if ([elementName isEqualToString:kElementPeriod]) {
+  } else if ([elementName isEqualToString:kElementMPD]) {
+    if ([_openElements count] > 1 ||
+        [[_openElements lastObject] isEqualToString:kElementMPD] == false) {
+      _parseFailed = true;
+      return;
+    }
+    [_openElements removeLastObject];
+  } else {
+    _parseFailed = true;
+    NSLog(@"didEndElement set _parseFailed on unexpected open element (%@)",
+          elementName);
+  }
 }
 
 - (void)parserDidEndDocument:(NSXMLParser*)parser {
