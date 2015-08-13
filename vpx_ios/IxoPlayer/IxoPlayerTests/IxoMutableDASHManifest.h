1636117ddbce5c9456a85287115ad35eb4aaa899
Tom Finegan <tomfinegan@google.com>
2015-08-12 17:29:17 -0700

    IxoPlayer: Add mutable versions of manifest data objects.
    When testing it's convenient to be able to have hard coded parse
    result objects to compare to actual parsing results.
    
    Change-Id: Ie7a717161bb4f5f6812389a880b5d46e32edbc58

---
+++ vpx_ios/IxoPlayer/IxoPlayerTests/IxoMutableDASHManifest.h
@@ -0,0 +1,61 @@
+// Copyright (c) 2015 The WebM project authors. All Rights Reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree. An additional intellectual property rights grant can be found
+// in the file PATENTS.  All contributing project authors may
+// be found in the AUTHORS file in the root of the source tree.
+#import "IxoDASHManifestParser.h"
+
+//
+// Mutable versions of the IxoDASHManifestParser data classes for testing.
+//
+
+@interface IxoMutableDASHRepresentation : IxoDASHRepresentation<NSCopying>
+@property(nonatomic, strong) NSString* repID;
+@property(nonatomic) int bandwidth;
+@property(nonatomic) int width;
+@property(nonatomic) int height;
+@property(nonatomic) int audioSamplingRate;
+@property(nonatomic) int audioChannelConfig;
+@property(nonatomic) int startWithSAP;
+@property(nonatomic, strong) NSString* baseURL;
+@property(nonatomic, strong) NSString* codecs;
+@property(nonatomic, strong) NSArray* segmentBaseIndexRange;
+@property(nonatomic, strong) NSArray* initializationRange;
+
+- (id)init;
+@end
+
+@interface IxoMutableDASHAdaptationSet : IxoDASHAdaptationSet
+@property(nonatomic, strong) NSString* setID;
+@property(nonatomic, strong) NSString* mimeType;
+@property(nonatomic, strong) NSString* codecs;
+@property(nonatomic) bool subsegmentAlignment;
+@property(nonatomic) bool bitstreamSwitching;
+@property(nonatomic) int subsegmentStartsWithSAP;
+@property(nonatomic) int width;
+@property(nonatomic) int height;
+@property(nonatomic) int audioSamplingRate;
+@property(nonatomic, strong) NSMutableArray* representations;
+
+- (id)init;
+@end
+
+@interface IxoMutableDASHPeriod : IxoDASHPeriod
+@property(nonatomic, strong) NSString* periodID;
+@property(nonatomic, strong) NSString* start;
+@property(nonatomic, strong) NSString* duration;
+@property(nonatomic, strong) NSMutableArray* audioAdaptationSets;
+@property(nonatomic, strong) NSMutableArray* videoAdaptationSets;
+
+- (id)init;
+@end
+
+@interface IxoMutableDASHManifest : IxoDASHManifest
+@property(nonatomic, strong) NSString* mediaPresentationDuration;
+@property(nonatomic, strong) NSString* minBufferTime;
+@property(nonatomic) bool staticPresentation;
+@property(nonatomic, strong) IxoMutableDASHPeriod* period;
+- (id)init;
+@end
---
+++ vpx_ios/IxoPlayer/IxoPlayerTests/IxoMutableDASHManifest.m
@@ -0,0 +1,140 @@
+// Copyright (c) 2015 The WebM project authors. All Rights Reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree. An additional intellectual property rights grant can be found
+// in the file PATENTS.  All contributing project authors may
+// be found in the AUTHORS file in the root of the source tree.
+#import "IxoMutableDASHManifest.h"
+
+//
+// IxoMutableDASHRepresentation
+//
+@implementation IxoMutableDASHRepresentation
+@synthesize repID = _repID;
+@synthesize bandwidth = _bandwidth;
+@synthesize width = _width;
+@synthesize height = _height;
+@synthesize audioSamplingRate = _audioSamplingRate;
+@synthesize audioChannelConfig = _audioChannelConfig;
+@synthesize startWithSAP = _startWithSAP;
+@synthesize codecs = _codecs;
+@synthesize baseURL = _baseURL;
+@synthesize segmentBaseIndexRange = _segmentBaseIndexRange;
+@synthesize initializationRange = _initializationRange;
+
+- (id)init {
+  self = [super init];
+  if (self) {
+    self.repID = nil;
+    self.bandwidth = 0;
+    self.width = 0;
+    self.height = 0;
+    self.audioSamplingRate = 0;
+    self.audioChannelConfig = 0;
+    self.startWithSAP = 0;
+    self.codecs = nil;
+    self.baseURL = nil;
+    self.segmentBaseIndexRange = nil;
+    self.initializationRange = nil;
+  }
+  return self;
+}
+
+- (id)copyWithZone:(NSZone *)zone {
+  IxoMutableDASHRepresentation* copy =
+      [[IxoMutableDASHRepresentation alloc] init];
+  if (copy) {
+    copy.repID = self.repID;
+    copy.bandwidth = self.bandwidth;
+    copy.width = self.width;
+    copy.height = self.height;
+    copy.audioSamplingRate = self.audioSamplingRate;
+    copy.audioChannelConfig = self.audioChannelConfig;
+    copy.startWithSAP = self.startWithSAP;
+    copy.codecs = self.codecs;
+    copy.baseURL = self.baseURL;
+    copy.segmentBaseIndexRange = self.segmentBaseIndexRange;
+    copy.initializationRange = self.initializationRange;
+  }
+  return copy;
+}
+@end
+
+//
+// IxoMutableDASHAdaptationSet
+//
+@implementation IxoMutableDASHAdaptationSet
+@synthesize setID = _setID;
+@synthesize mimeType = _mimeType;
+@synthesize codecs = _codecs;
+@synthesize subsegmentAlignment = _subsegmentAlignment;
+@synthesize bitstreamSwitching = _bitstreamSwitching;
+@synthesize subsegmentStartsWithSAP = _subsegmentStartsWithSAP;
+@synthesize width = _width;
+@synthesize height = _height;
+@synthesize audioSamplingRate = _audioSamplingRate;
+@synthesize representations = _representations;
+
+- (id)init {
+  self = [super init];
+  if (self) {
+    _setID = nil;
+    //    self.setID = nil;
+    self.mimeType = nil;
+    self.codecs = nil;
+    self.subsegmentAlignment = false;
+    self.bitstreamSwitching = false;
+    self.subsegmentStartsWithSAP = 0;
+    self.width = 0;
+    self.height = 0;
+    self.audioSamplingRate = 0;
+    self.representations = nil;
+  }
+  return self;
+}
+@end
+
+//
+// IxoMutableDASHPeriod
+//
+@implementation IxoMutableDASHPeriod
+@synthesize periodID = _periodID;
+@synthesize start = _start;
+@synthesize duration = _duration;
+@synthesize audioAdaptationSets = _audioAdaptationSets;
+@synthesize videoAdaptationSets = _videoAdaptationSets;
+
+- (id)init {
+  self = [super init];
+  if (self) {
+    self.periodID = nil;
+    self.start = nil;
+    self.duration = nil;
+    self.audioAdaptationSets = nil;
+    self.videoAdaptationSets = nil;
+  }
+  return self;
+}
+@end
+
+//
+// IxoMutableDASHManifest
+//
+@implementation IxoMutableDASHManifest
+@synthesize mediaPresentationDuration = _mediaPresentationDuration;
+@synthesize minBufferTime = _minBufferTime;
+@synthesize staticPresentation = _staticPresentation;
+@synthesize period = _period;
+
+- (id)init {
+  self = [super init];
+  if (self) {
+    self.mediaPresentationDuration = nil;
+    self.minBufferTime = nil;
+    self.staticPresentation = false;
+    self.period = nil;
+  }
+  return self;
+}
+@end
