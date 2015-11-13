078216d1ac208f0272a4c9dd300ca1533fb3f5bf
Tom Finegan <tomfinegan@google.com>
2015-11-13 13:38:28 -0800

    IxoDASHManifestParser: Add convenience accessors for representations.
    Make it easier to obtain the representations array for a given
    adaptation set.
    
    Change-Id: I546351568db5dfabae1646511fc9774ea4d9bfe9

--- vpx_ios/IxoPlayer/IxoDASHManifestParser.m
+++ vpx_ios/IxoPlayer/IxoDASHManifestParser.m
@@ -271,8 +271,22 @@
   return true;
 }
 
-- (NSString*)absoluteURLStringForBaseURLString:(NSString *)baseURLString {
+- (NSString*)absoluteURLStringForBaseURLString:(NSString*)baseURLString {
   return [NSString stringWithFormat:@"%@%@", _manifestPath, baseURLString, nil];
+}
+
+- (NSMutableArray*)audioRepsForAdaptationSetWithIndex:(int)index {
+  if (index >= _manifest.period.audioAdaptationSets.count)
+    return nil;
+  return [[_manifest.period.audioAdaptationSets
+      objectAtIndex:index] representations];
+}
+
+- (NSMutableArray*)videoRepsForAdaptationSetWithIndex:(int)index {
+  if (index >= _manifest.period.videoAdaptationSets.count)
+    return nil;
+  return [[_manifest.period.videoAdaptationSets
+      objectAtIndex:index] representations];
 }
 
 - (bool)adaptationSetIsValid:(IxoDASHAdaptationSet*)set {
@@ -290,10 +304,9 @@
 - (bool)canPlayFromParsedManifest {
   // The manifest and period must be non-nil, and at least one adaptation set
   // must be non-empty.
-  if (_manifest == nil ||
-      _manifest.period == nil ||
+  if (_manifest == nil || _manifest.period == nil ||
       (_manifest.period.audioAdaptationSets.count == 0 &&
-          _manifest.period.videoAdaptationSets.count == 0)) {
+       _manifest.period.videoAdaptationSets.count == 0)) {
     return false;
   }
 
@@ -306,7 +319,7 @@
 
   if (_manifest.period.videoAdaptationSets.count > 0) {
     for (IxoDASHAdaptationSet* set in _manifest.period.videoAdaptationSets) {
-       if ([self adaptationSetIsValid:set] == false)
+      if ([self adaptationSetIsValid:set] == false)
         return false;
     }
   }
@@ -367,8 +380,8 @@
 - (NSString*)removeFileNameFromURLString:(NSString*)string {
   NSRange fname_range = [string rangeOfString:@"/" options:NSBackwardsSearch];
   fname_range.length = string.length - fname_range.location;
-  return [string stringByReplacingCharactersInRange:fname_range
-                                         withString:@"/"];
+  return
+      [string stringByReplacingCharactersInRange:fname_range withString:@"/"];
 }
 
 //
